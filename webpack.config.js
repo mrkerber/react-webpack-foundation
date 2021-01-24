91a1c4188364171a27438a62f77adf906be1b603
Matthew Kerber <mrkerber.cs@gmail.com>
2021-01-24 00:50:28 -0500

    Webpack Config devops optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
-const path = require('path');
+const Path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require('webpack');
+const Webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = function(_env, argv) {
   const isProduction = argv.mode === "production";
@@ -75,6 +77,49 @@ module.exports = function(_env, argv) {
         inject: true
       })
     ].filter(Boolean),
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserWebpackPlugin({
+          terserOptions: {
+            compress: {
+              comparisons: false
+            },
+            mangle: {
+              safari10: true
+            },
+            output: {
+              comments: false,
+              ascii_only: true
+            },
+            warnings: false
+          }
+        }),
+        new OptimizeCssAssetsPlugin()
+      ],
+      splitChunks: {
+        chunks: "all",
+        minSize: 0,
+        maxInitialRequests: 20,
+        maxAsyncRequests: 20,
+        cacheGroups: {
+          vendors: {
+            test: /[\\/]noe_modules[\\/]/,
+            name(module, chunks, cacheGroupKey) {
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1];
+              return '${cacheGroupKey}.${packageName.replace("@", "")}';
+            }
+          },
+          common: {
+            minChunks: 2,
+            priority: -10
+          }
+        }
+      },
+      runtimeChunk:"single"
+    },
     resolve: {
       extensions: [".js", ".jsx"]
     },
@@ -83,5 +128,5 @@ module.exports = function(_env, argv) {
       aggregateTimeout: 200,
       poll: 1000
     }
-  }
-}
+  };
+};
